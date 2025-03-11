2197ec9b1bce9738bfcd24d5cbf95c8934df4c9f
pranavyey <e1155906@u.nus.edu>
2025-03-11 16:38:42 +0800

    Update execute method in PinCommand class to use the last person listing

--- src/main/java/seedu/address/logic/commands/PinCommand.java
+++ src/main/java/seedu/address/logic/commands/PinCommand.java
@@ -1,25 +1,38 @@
 package seedu.address.logic.commands;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 public class PinCommand extends Command {
 
     public static final String COMMAND_WORD = "pin";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Pins the person identified by the index number used in the displayed person list.\n"
+            + ": Pins the person identified by the index number used in the last person listing.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    private final Index targetIndex;
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Pin command not implemented yet";
 
-    public PinCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d";
+
+    private final Index index;
+
+    public PinCommand(Index index) {
+
+        this.index = index;
     }
 
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Pin logic not implemented");
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
