78a1dc0edc9d72253e4b738af69f0bde7757ae34
pranavyey <e1155906@u.nus.edu>
2025-03-11 19:07:24 +0800

    Add tests to cover new functionality

--- src/test/java/seedu/address/logic/commands/PinCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PinCommandTest.java
@@ -1,6 +1,6 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.PinCommand.MESSAGE_ARGUMENTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -8,9 +8,13 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -21,9 +25,23 @@ public class PinCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute() {
-        assertCommandFailure(new PinCommand(INDEX_FIRST_PERSON), model,
-                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased()));
+    public void execute_validIndex_pinsPerson() throws Exception {
+        Person personToPin = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        PinCommand pinCommand = new PinCommand(INDEX_FIRST_PERSON);
+
+        assertThrows(CommandException.class, () -> pinCommand.execute(model));
+
+        // Ensure that person is now at index 0
+        assertEquals(personToPin, model.getFilteredPersonList().get(0));
+    }
+
+    @Test
+    public void execute_invalidIndex_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        PinCommand pinCommand = new PinCommand(outOfBoundIndex);
+
+        assertThrows(CommandException.class, () -> pinCommand.execute(model),
+                Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
 
