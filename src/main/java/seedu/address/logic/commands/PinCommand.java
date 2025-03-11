3484e8233220b7f820666096415b8e4ac776fd5a
pranavyey <e1155906@u.nus.edu>
2025-03-11 16:18:37 +0800

    Add PinCommandParser class in parser package

--- src/main/java/seedu/address/logic/commands/PinCommand.java
+++ src/main/java/seedu/address/logic/commands/PinCommand.java
@@ -1,10 +1,22 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 
 public class PinCommand extends Command {
 
     public static final String COMMAND_WORD = "pin";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Pins the person identified by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    private final Index targetIndex;
+
+    public PinCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
 
     @Override
     public CommandResult execute(Model model) {
