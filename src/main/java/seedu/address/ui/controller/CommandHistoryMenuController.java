15baea17a88298909275e803b8ba831eaf9b292f
Chen Linxin <chen_ll@u.nus.edu>
2025-03-16 17:28:04 +0800

    Update history selection logic

--- src/main/java/seedu/address/ui/controller/CommandHistoryMenuController.java
+++ src/main/java/seedu/address/ui/controller/CommandHistoryMenuController.java
@@ -1,22 +1,28 @@
 package seedu.address.ui.controller;
 
+import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import seedu.address.commons.core.LogsCenter;
 
 /**
  * Represents the controller for the command history menu ui component.
  */
 public final class CommandHistoryMenuController {
     static final int DEFAULT_SELECTION_INDEX = 0;
+    private static final Logger logger = LogsCenter.getLogger(CommandHistoryMenuController.class);
 
     final ObservableList<String> pastCommands;
     final CommandBoxInputSetter commandSetter;
 
-    private int commandSelectionIndex = DEFAULT_SELECTION_INDEX;
+
+    private Optional<Integer> commandSelectionIndex;
 
     /**
      * Creates a {@code CommandHistoryMenuController}
@@ -29,17 +35,8 @@ public final class CommandHistoryMenuController {
             ObservableList<String> commandHistory, CommandBoxInputSetter commandSetter) {
         this.commandSetter = commandSetter;
         pastCommands = commandHistory;
-    }
-
-    /**
-     * Moves the command history selection up.
-     */
-    public void moveUp() {
-        if (pastCommands.isEmpty()) {
-            return;
-        }
-        commandSelectionIndex = (commandSelectionIndex + 1) % pastCommands.size();
-        setInputToSelection();
+        // initially, none are selected
+        commandSelectionIndex = Optional.empty();
     }
 
     /**
@@ -49,22 +46,46 @@ public final class CommandHistoryMenuController {
         if (pastCommands.isEmpty()) {
             return;
         }
-        commandSelectionIndex = (pastCommands.size() + commandSelectionIndex - 1) % pastCommands.size();
-        assert commandSelectionIndex >= 0 && commandSelectionIndex < pastCommands.size();
+        commandSelectionIndex = commandSelectionIndex
+                .map(previousSelectionIndex -> (previousSelectionIndex + 1) % pastCommands.size())
+                .or(() -> Optional.of(DEFAULT_SELECTION_INDEX));
         setInputToSelection();
     }
+
     /**
-     * Resets the selected input to the default index value.
+     * Moves the command history selection up.
      */
-    public void resetSelection() {
-        commandSelectionIndex = DEFAULT_SELECTION_INDEX;
+    public void moveUp() {
+        if (pastCommands.isEmpty()) {
+            return;
+        }
+        commandSelectionIndex = commandSelectionIndex
+                .map(previousSelectionIndex
+                        -> (pastCommands.size() + previousSelectionIndex - 1) % pastCommands.size())
+                .or(() -> Optional.of(DEFAULT_SELECTION_INDEX));
+        setInputToSelection();
+    }
+
+    public void setSelection(int commandSelectionIndex) {
+        logger.log(Level.INFO, "Got selection index: " + commandSelectionIndex);
+        if (commandSelectionIndex < 0 || commandSelectionIndex >= pastCommands.size()) {
+            return;
+        }
+        this.commandSelectionIndex = Optional.of(commandSelectionIndex);
+        setInputToSelection();
+    }
+
+    public void clearSelection() {
+        commandSelectionIndex = Optional.empty();
     }
 
     private void setInputToSelection() {
-        commandSetter.setCommandInput(pastCommands.get(commandSelectionIndex));
+        commandSelectionIndex.ifPresent(index -> {
+            commandSetter.setCommandInput(pastCommands.get(index));
+        });
     }
 
-    int getCommandSelectionIndex() {
+    public Optional<Integer> getCommandSelectionIndex() {
         return commandSelectionIndex;
     }
 
@@ -80,6 +101,7 @@ public final class CommandHistoryMenuController {
             boolean isPageUpAndCtrlDown = event.getCode() == KeyCode.UP && event.isControlDown();
             boolean isPageDownAndCtrlDown = event.getCode() == KeyCode.DOWN && event.isControlDown();
             boolean isEnterPressed = event.getCode() == KeyCode.ENTER;
+            boolean isEscPressed = event.getCode() == KeyCode.ESCAPE;
 
             if (isPageUpAndCtrlDown) {
                 handlerSupplier.get().handleMovementUp();
@@ -91,8 +113,8 @@ public final class CommandHistoryMenuController {
                 event.consume();
                 return;
             }
-            if (isEnterPressed) {
-                handlerSupplier.get().handleEnterPressed();
+            if (isEnterPressed || isEscPressed) {
+                handlerSupplier.get().handleCloseAction();
             }
         };
     }
