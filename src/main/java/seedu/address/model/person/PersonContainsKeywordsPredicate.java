4bba16a927d8e62f1e1fac75894a52f486b17870
xinniyee <xinniyee@gmail.com>
2025-03-18 15:12:30 +0800

    Change variable name

--- src/main/java/seedu/address/model/person/PersonContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/PersonContainsKeywordsPredicate.java
@@ -12,43 +12,43 @@ import seedu.address.commons.util.ToStringBuilder;
  */
 public class PersonContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
-    private final boolean searchName;
-    private final boolean searchPhone;
-    private final boolean searchEmail;
-    private final boolean searchAddress;
-    private final boolean searchTags;
+    private final boolean isSearchByName;
+    private final boolean isSearchByPhone;
+    private final boolean isSearchByEmail;
+    private final boolean isSearchByAddress;
+    private final boolean isSearchByTags;
 
     /**
      * Constructs a {@code PersonContainsKeywordsPredicate} with the specified search parameters.
      *
      * @param keywords A list of keywords to search for.
-     * @param searchName {@code true} if the search should match names, {@code false} otherwise.
-     * @param searchPhone {@code true} if the search should match phone numbers, {@code false} otherwise.
-     * @param searchEmail {@code true} if the search should match emails, {@code false} otherwise.
-     * @param searchAddress {@code true} if the search should match addresses, {@code false} otherwise.
-     * @param searchTags {@code true} if the search should match tags, {@code false} otherwise.
+     * @param isSearchByName {@code true} if the search should match names, {@code false} otherwise.
+     * @param isSearchByPhone {@code true} if the search should match phone numbers, {@code false} otherwise.
+     * @param isSearchByEmail {@code true} if the search should match emails, {@code false} otherwise.
+     * @param isSearchByAddress {@code true} if the search should match addresses, {@code false} otherwise.
+     * @param isSearchByTags {@code true} if the search should match tags, {@code false} otherwise.
      */
-    public PersonContainsKeywordsPredicate(List<String> keywords, boolean searchName, boolean searchPhone,
-                                           boolean searchEmail, boolean searchAddress, boolean searchTags) {
+    public PersonContainsKeywordsPredicate(List<String> keywords, boolean isSearchByName, boolean isSearchByPhone,
+                                           boolean isSearchByEmail, boolean isSearchByAddress, boolean isSearchByTags) {
         this.keywords = keywords;
-        this.searchName = searchName;
-        this.searchPhone = searchPhone;
-        this.searchEmail = searchEmail;
-        this.searchAddress = searchAddress;
-        this.searchTags = searchTags;
+        this.isSearchByName = isSearchByName;
+        this.isSearchByPhone = isSearchByPhone;
+        this.isSearchByEmail = isSearchByEmail;
+        this.isSearchByAddress = isSearchByAddress;
+        this.isSearchByTags = isSearchByTags;
     }
 
     @Override
     public boolean test(Person person) {
-        return (searchName && keywords.stream()
+        return (isSearchByName && keywords.stream()
                 .anyMatch(keyword -> StringUtil.containsPhraseIgnoreCase(person.getName().fullName, keyword)))
-                || (searchPhone && keywords.stream()
+                || (isSearchByPhone && keywords.stream()
                 .anyMatch(keyword -> StringUtil.containsPhraseIgnoreCase(person.getPhone().value, keyword)))
-                || (searchEmail && keywords.stream()
+                || (isSearchByEmail && keywords.stream()
                 .anyMatch(keyword -> StringUtil.containsPhraseIgnoreCase(person.getEmail().value, keyword)))
-                || (searchAddress && keywords.stream()
+                || (isSearchByAddress && keywords.stream()
                 .anyMatch(keyword -> StringUtil.containsPhraseIgnoreCase(person.getAddress().value, keyword)))
-                || (searchTags && person.getTags().stream()
+                || (isSearchByTags && person.getTags().stream()
                 .anyMatch(tag -> keywords.stream()
                         .anyMatch(keyword -> StringUtil.containsPhraseIgnoreCase(tag.tagName, keyword))));
     }
@@ -66,21 +66,21 @@ public class PersonContainsKeywordsPredicate implements Predicate<Person> {
 
         PersonContainsKeywordsPredicate otherPredicate = (PersonContainsKeywordsPredicate) other;
         return keywords.equals(otherPredicate.keywords)
-                && searchName == otherPredicate.searchName
-                && searchPhone == otherPredicate.searchPhone
-                && searchEmail == otherPredicate.searchEmail
-                && searchAddress == otherPredicate.searchAddress
-                && searchTags == otherPredicate.searchTags;
+                && isSearchByName == otherPredicate.isSearchByName
+                && isSearchByPhone == otherPredicate.isSearchByPhone
+                && isSearchByEmail == otherPredicate.isSearchByEmail
+                && isSearchByAddress == otherPredicate.isSearchByAddress
+                && isSearchByTags == otherPredicate.isSearchByTags;
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this).add("keywords", keywords)
-                .add("searchName", searchName)
-                .add("searchPhone", searchPhone)
-                .add("searchEmail", searchEmail)
-                .add("searchAddress", searchAddress)
-                .add("searchTags", searchTags)
+                .add("searchName", isSearchByName)
+                .add("searchPhone", isSearchByPhone)
+                .add("searchEmail", isSearchByEmail)
+                .add("searchAddress", isSearchByAddress)
+                .add("searchTags", isSearchByTags)
                 .toString();
     }
 }
