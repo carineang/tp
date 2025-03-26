037734e380615f532858952fba279feb69269d71
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-03-27 03:51:00 +0800

    Add RedoCommand and UndoCommand test cases

--- src/test/java/seedu/address/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -1,4 +1,33 @@
 package seedu.address.logic.commands;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.CARL;
+
 public class RedoCommandTest {
+
+    @Test
+    public void execute_RedosLeft_success() {
+        Model model = new ModelManager();
+        model.addPerson(CARL);
+        model.undoAddressBook();
+        Model expectedModel = new ModelManager();
+        expectedModel.addPerson(CARL);
+        expectedModel.undoAddressBook();
+        expectedModel.redoAddressBook();
+
+        assertCommandSuccess(new RedoCommand(), model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_noUndosLeft_failure() {
+        Model model = new ModelManager();
+
+        assertCommandFailure(new RedoCommand(), model, Messages.MESSAGE_INVALID_NO_REDO);
+    }
 }
--- src/test/java/seedu/address/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -1,10 +1,33 @@
 package seedu.address.logic.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
+import java.security.MessageDigest;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.CARL;
 
 public class UndoCommandTest {
+
+    @Test
+    public void execute_UndosLeft_success() {
+        Model model = new ModelManager();
+        model.addPerson(CARL);
+        Model expectedModel = new ModelManager();
+        expectedModel.addPerson(CARL);
+        expectedModel.undoAddressBook();
+
+        assertCommandSuccess(new UndoCommand(), model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_noUndosLeft_failure() {
+        Model model = new ModelManager();
+
+        assertCommandFailure(new UndoCommand(), model, Messages.MESSAGE_INVALID_NO_UNDO);
+    }
 }
