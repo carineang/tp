67a60863903b6c5b535c6e44a657aa8ad7460b91
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-03-27 03:07:41 +0800

    Add initial test files

---
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+public class UndoCommandTest {
+}
