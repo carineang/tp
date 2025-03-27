7bc47b7898e30d3c1a90b5c5c3efeef71bbbc4c1
pranavyey <e1155906@u.nus.edu>
2025-03-27 23:06:11 +0800

    Fix import order

--- src/test/java/seedu/address/logic/commands/UnpinCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnpinCommandTest.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.commands;
 
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
