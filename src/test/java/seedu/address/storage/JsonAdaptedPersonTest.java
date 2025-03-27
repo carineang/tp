a8f32c4bb8bcda19c6340f331af64c0fba80e1f5
pranavyey <e1155906@u.nus.edu>
2025-03-27 13:29:20 +0800

    Remove unused import

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -17,7 +17,6 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Note;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Pin;
 
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
