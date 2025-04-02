68107f03028c29344132146bcf477a8647ff2db4
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-04-03 03:04:10 +0800

    Update javadocs

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -81,6 +81,7 @@ public interface Model {
 
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
+     *
      * @throws NullPointerException if {@code predicate} is null.
      */
     void updateFilteredPersonList(Predicate<Person> predicate);
@@ -127,33 +128,33 @@ public interface Model {
     void updateSortedFilteredPersonList(String... prefix);
 
     /**
-     * Saves the state of the address book
-     * It is up to the user of this method to decide when to commit the address book
+     * Saves the state of the model
+     * It is up to the user of this method to decide when to commit the model
      *
      */
     void commit();
 
     /**
-     * Restores the state of the address book to the last saved state
+     * Restores the state of the model to the last saved state
      *
      * @throws IndexOutOfBoundsException if there is no last saved state
      */
     void undo();
 
     /**
-     * Restores the state of the address book to the last saved undone state
+     * Restores the state of the model model to the last saved undone model state
      *
      * @throws IndexOutOfBoundsException if there is no last saved state
      */
     void redo();
 
     /**
-     * Checks if the address book has a state to undo
+     * Checks if the model has a state to undo
      */
     boolean hasUndo();
 
     /**
-     * Checks if the address book has an undone state to redo
+     * Checks if the model has an undone state to redo
      */
     boolean hasRedo();
 }
