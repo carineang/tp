35ca40913f33c8596a1ac0459e3f19745fcc604c
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-04-02 12:29:25 +0800

    Add test case for duplicate nt/ bug

--- src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/NoteCommandParserTest.java
@@ -1,14 +1,17 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.getErrorMessageForDuplicatePrefixes;
 import static seedu.address.logic.commands.CommandTestUtil.NOTE_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.NOTE_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NOTE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NOTE_BOB;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 import org.junit.jupiter.api.Test;
@@ -65,4 +68,13 @@ public class NoteCommandParserTest {
         assertParseSuccess(parser, userInput, expectedCommand);
     }
 
+    @Test
+    public void parse_duplicateNotePrefixes_failure() {
+        String invalidCommandInput = "0"
+                + NOTE_DESC_AMY
+                + NOTE_DESC_BOB;
+
+        assertParseFailure(parser, invalidCommandInput,
+                getErrorMessageForDuplicatePrefixes(PREFIX_NOTE));
+    }
 }
