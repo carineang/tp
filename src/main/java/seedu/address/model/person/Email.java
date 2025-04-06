bb5066892254380252fc5f487d5121572b479503
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-04-06 20:47:33 +0800

    Fix email usage message

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -15,6 +15,7 @@ public class Email {
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
             + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
             + "characters.\n"
+            + "A special character must be followed by at least one alphanumeric character.\n"
             + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
             + "separated by periods.\n"
             + "The domain name must:\n"
